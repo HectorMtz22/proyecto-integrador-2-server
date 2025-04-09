c7973ca0eb71f9cb88ad74bd17501a30721dd454
Hector Martinez <hec.gre81@gmail.com>
2025-04-09 23:32:40 +0000

    Add 2 plots for Egresados-Titulados and 6to oportunity

--- read_file.py
+++ read_file.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 file_data_location = 'data/R3DET 8.0.xlsx'
 file_sheet = "TG"
 
@@ -16,3 +17,33 @@ filtered_data = filtered_data[filtered_data['Cohorte'].str.lower() == semestre.l
 
 # Display the filtered data
 print(filtered_data)
+
+# Plotting the data
+# First plot Egresados and Titulados
+plt.figure(figsize=(10, 5))
+plt.plot(filtered_data['Per'], filtered_data['Eg'], marker='o', color='blue')
+plt.plot(filtered_data['Per'], filtered_data['STit'], marker='o', color='green')
+plt.legend(['Egresados', 'Titulados'], loc='upper right')
+plt.title(f"Cantidad de alumnos ITS del periodo {semestre.upper()}")
+plt.xlabel('Periodo')
+plt.ylabel('Cantidad')
+plt.tight_layout()
+plt.gca().yaxis.get_major_locator().set_params(integer=True)
+
+# Save image
+plt.savefig(f"images/good_{semestre}.png")
+
+# New plot
+# Cambios de carrera y 6ta oportunidad
+plt.figure(figsize=(10, 5))
+plt.plot(filtered_data['Per'], filtered_data['ECC6'], marker='o', color='red')
+plt.plot(filtered_data['Per'], filtered_data['ECC'], marker='o', color='orange')
+
+plt.gca().yaxis.get_major_locator().set_params(integer=True)
+plt.legend(['Cambio por 6ta oportunidad', 'Cambio de carrera'], loc='upper right')
+plt.title(f"Cantidad de alumnos ITS del periodo {semestre.upper()}")
+plt.xlabel('Periodo')
+plt.ylabel('Cantidad')
+plt.tight_layout()
+# Save image
+plt.savefig(f"images/bad_{semestre}.png")
